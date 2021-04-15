675a4b1e3455905ed889c1d39d07ec7eaff44bf7
Yuichi <yuichi.888@outlook.jp>
2021-04-15 20:41:45 +0800

    add url

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -32,7 +32,7 @@ export default {
   methods: {
     auth() {
       axios
-        .post("herokuのURL/api/register", {
+        .post("https://infinite-atoll-83522.herokuapp.com/api/register", {
           name: this.name,
           profile: this.profile,
           email: this.email,
